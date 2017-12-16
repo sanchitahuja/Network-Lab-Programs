c2d0543101f41c10ce3c0f683fe65bedb0da7b7c
sanchitahuja <sanchit.ahuja.97@gmail.com>
2017-12-16 23:58:36 +0530

    RSA Java Updated

--- RSAEncryption/RSAEncryptionDemo.java
+++ RSAEncryption/RSAEncryptionDemo.java
@@ -1,5 +1,7 @@
 package RSAEncryption;
 
+import java.util.Scanner;
+
 public class RSAEncryptionDemo {
 
 public static boolean isPrime(int number){
@@ -39,10 +41,55 @@ public static boolean isPrime(int number){
         }
 
     }
+    public static double encryptor(int message,int publickeyexponent,int publickeymodulus){
+        return Math.pow(message,publickeyexponent)%publickeymodulus;
+    }
 
-
-
-
+    public static double decryptor(int message,int privateexponent,int privatemodulus){
+        return Math.pow(message,privateexponent)%privatemodulus;
+    }
+    public static void main(String args[]){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter a prime number P");
+        int p=scanner.nextInt();
+        if(!isPrime(p)){
+            while (true){
+                System.out.println("Not Prime\nEnter a prime number P");
+                p=scanner.nextInt();
+                if(isPrime(p))
+                    break;
+            }
+        }
+        System.out.println("Enter a prime number Q");
+        int q=scanner.nextInt();
+        if(!isPrime(q)){
+            while (true){
+                System.out.println("Not Prime\nEnter a prime number Q");
+                q=scanner.nextInt();
+                if(isPrime(q))
+                    break;
+            }
+        }
+        int modulus=p*q;
+        int totient=(p-1)*(q-1);
+        System.out.println("Enter Public Key Exponent");
+        int publicKeyExponent=scanner.nextInt();
+        while (!isCoPrime(publicKeyExponent,totient)){
+            System.out.println("Totient and Public key arent coprime");
+            System.out.println("Enter Public Key Exponent");
+            publicKeyExponent=scanner.nextInt();
+            if(isCoPrime(publicKeyExponent,totient))
+                break;
+        }
+        System.out.println("totient "+totient);
+        int privateKeyExponent=dCalculator(publicKeyExponent,totient);
+        System.out.println("Public Key Modulus "+modulus+" Exponent "+publicKeyExponent);
+        System.out.println("Private Key Modulus "+modulus+" Exponent "+privateKeyExponent);
+        System.out.println("Enter Message");
+        int message=scanner.nextInt();
+        System.out.println("Encrypted Message "+encryptor(message,publicKeyExponent,modulus));
+        System.out.println("Decrypted Message "+decryptor(message,privateKeyExponent,modulus));
+    }
 
 
 }
